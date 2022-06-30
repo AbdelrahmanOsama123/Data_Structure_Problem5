f0b29fd28beec868bfc2046b7ef87ee13cdf9cc2
Abdelrahman Osama <61732484+AbdelrahmanOsama123@users.noreply.github.com>
2022-06-30 17:51:58 +0200

    Add files via upload

---
+++ myQueue.h
@@ -0,0 +1,16 @@
+#ifndef MYQUEUE_H
+#define MYQUEUE_H
+
+
+class myQueue
+{
+    public:
+        myQueue();
+        virtual ~myQueue();
+
+    protected:
+
+    private:
+};
+
+#endif // MYQUEUE_H
